0cb2e5802490e1e4fe048addeecd60ec3265b1b4
CI Pusher <support@unipi.technology>
2026-04-01 17:22:22 +0000

    dts: fix missing spi overlay on UDELAB0602 fix: Ledproxy is optional from now fix: Swap E414/E415 definitions

--- files/usr/lib/unipi/fwi2c-check.sh
+++ files/usr/lib/unipi/fwi2c-check.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+UNIPI_PRODUCT_ID="$(cat /run/unipi-plc/unipi-id/platform_id)"
+LEDFILE="/usr/share/unipi-os-configurator/ledmaps/${UNIPI_PRODUCT_ID:-.unknown.}"
+
+if ! test "$LEDFILE"; then
+  # it is OK to finish with board without ledproxy
+  echo 'WARNING led proxy map not defined for this product.'
+  exit 0
+fi
+
 if fwi2c | grep -q 'Bootloader only mode'; then
     echo 'ERROR fwi2c in bootloader mode'
     exit 1
@@ -8,8 +17,6 @@ if ! /usr/bin/fwi2c --read-conf=/tmp/0000 >/dev/null 2>&1; then
     echo 'ERROR fwi2c --read-conf'
     exit 1
 fi
-UNIPI_PRODUCT_ID="$(cat /run/unipi-plc/unipi-id/platform_id)"
-LEDFILE="/usr/share/unipi-os-configurator/ledmaps/${UNIPI_PRODUCT_ID:-.unknown.}"
 if ! diff /tmp/0000 "$LEDFILE"  >/dev/null; then
     rm -f /tmp/0000
     echo 'ERROR fwi2c conf incorrect'
